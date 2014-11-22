a312c49fcc43dcbf5aa6035c08c8b2550a0318bb
Dario D. Müller <dario-domi@hotmail.de>
2014-11-22 13:17:26 +0100

    Shader versuchen..

--- soundXgame/Shader.h
+++ soundXgame/Shader.h
@@ -5,12 +5,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "Connectable.h"
+#include <math.h>
 
-class Shader
+
+class Shader :
+	public IConnectable
 {
+private:
+	GLuint program;
+	// Attribute + Uniforms für Vertices und Farbe
+	GLint attribute_coord2d, attribute_v_color;
+	GLint uniform_alpha;
+	// Buffer für Vertices und Farben
+	GLuint vbo_triangle, vbo_triangle_colors;
+
+	int initResourcesShader();
+	void drawShader();
+	void freeResources();
 public:
 	Shader(void);
 	~Shader(void);
+	virtual bool Initialize(void);
 	static char* readFile(const char* filename);
 	static GLuint createShader(const char* filename, GLenum type);
 
